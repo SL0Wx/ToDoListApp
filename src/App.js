5ae5499c6686e3141819675e86fb64563fa0b5fa
SL0Wx <k.maciejski2004@wp.pl>
2023-03-09 13:42:02 +0100

    complete app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import List from './components/List';
+import NewToDo from './components/NewToDo';
+import Button from 'react-bootstrap/Button';
 import './App.css';
 
 function App() {
+  const [toDoList, setToDoList] = useState([]);
+  const [addNew, setAddNew] = useState(false);
+  const [edit, setEdit] = useState(false);
+  const [id, setId] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="overlay" style={{ display: (addNew || edit) ? "block" : "none" }}></div>
+      <div className="container">
+        <h1>Twoja lista zadań</h1>
+        <List toDoList={toDoList} setToDoList={setToDoList} edit={edit} setEdit={setEdit} />
+        <Button variant="success" className="addBtn" onClick={() => setAddNew(true)}>Utwórz nowe zadanie</Button>
+        {addNew === true && (
+          <NewToDo setAddNew={setAddNew} toDoList={toDoList} setToDoList={setToDoList} id={id} setId={setId} />
+        )}
+      </div>
     </div>
   );
 }
